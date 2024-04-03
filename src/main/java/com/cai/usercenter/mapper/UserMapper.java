d0f5f7f3bdea046a0f317b61a92aaa1d023f8144
itcast <chl18307598501@qq.com>
2024-04-03 20:49:32 +0800

    提交项目

---
+++ src/main/java/com/cai/usercenter/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.cai.usercenter.mapper;
+
+import com.cai.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
